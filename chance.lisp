8efb200e60403c6609eada052965071c1d03d90f
William Clifford <wobh@yahoo.com>
2023-03-12 14:23:39 -0400

    chance: setup sampling
    - sample elements from non-list sequences
      - just need to repeatedly choose element from sequence
    - sample items from lists
      - use naive "resevoir sampling" "algorithm R"
      - support sampling from alists and plists and trees
      - use sampling list functions for random choice from list
      - tree-walking trick from:
        https://lisptips.com/post/43404489000/the-tree-walkers-of-cl
    - sample mappings from hash-tables
      - use algorithm R again
      - use sampling hash-table function for random choice from hash-table
    - fix nshuffle so it no longer tries to get the size
      - had to treat lists and non-list sequences differently

--- chance.lisp
+++ chance.lisp
@@ -18,6 +18,21 @@
            #:random-aref
            #:random-nth
            #:random-elt)
+  (:export #:sample-string
+           #:sample-simple-vector
+           #:sample-array)
+  ;; list items, associations, properties, and nodes
+  (:export #:sample-list
+           #:sample-alist
+           #:sample-plist
+           #:sample-tree)
+  (:export #:random-list-item
+           #:random-alist-assoc
+           #:random-plist-property
+           #:random-tree-node)
+  ;; hash-table mappings
+  (:export #:sample-hash-table
+           #:random-hash-table-mapping)
   ;; shuffles
   (:export #:nshuffle
            #:shuffle)
@@ -111,17 +126,237 @@ Provides chance utilities."))
          (random limit random-state))))
 
 
+;;; Sample random elements in strings, vectors, arrays
+
+(defun sample-string (sample-to a-string
+                      &optional (random-state *random-state*))
+  "Return vector of random characters in `a-string'."
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type string a-string))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             with population-size = (length a-string)
+             for index below sample-size
+             do (setf (svref sample index)
+                      (char a-string
+                            (random population-size
+                                    random-state)))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+(defun sample-simple-vector (sample-to a-simple-vector
+                             &optional (random-state *random-state*))
+  "Return vector of random elements in `a-simple-vector'."
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type simple-vector a-simple-vector))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             with population-size = (length a-simple-vector)
+             for index below sample-size
+             do (setf (svref sample index)
+                      (svref a-simple-vector
+                             (random population-size
+                                     random-state)))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+(defun sample-array (sample-to an-array
+                     &optional (random-state *random-state*))
+  "Return vector of random elements in `an-array'."
+    (declare (type (or simple-vector (integer 1)) sample-to)
+             (type array an-array))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             with population-size = (array-total-size an-array)
+             for index below sample-size
+             do (setf (svref sample index)
+                      (row-major-aref an-array
+                                      (random population-size
+                                              random-state)))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+
+;;; Sample list items
+
+(defun sample-list (sample-to a-list
+                    &optional (random-state *random-state*))
+  "Return vector of random items in `a-list'."
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type list a-list))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             for counter from 0
+             for item in a-list
+             if (< counter sample-size)
+               do (setf (svref sample counter)
+                        item)
+             else
+               do (let ((index (random counter random-state)))
+                    (when (< index sample-size)
+                      (setf (svref sample index)
+                            item)))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+(defun sample-alist (sample-to an-alist
+                     &optional (random-state *random-state*))
+  "Return vector of random associations (key datum pairs) in `an-alist'."
+  (sample-list sample-to
+               an-alist
+               random-state))
+
+(defun sample-plist (sample-to a-plist
+                     &optional (random-state *random-state*))
+  "Return vector of random properties (indicator value pairs) in `a-plist'. "
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type list a-plist))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             for counter from 0
+             for property in a-plist by #'cddr
+             if (< counter sample-size)
+               do (setf (svref sample counter)
+                        property)
+             else
+               do (let ((index (random counter random-state)))
+                    (when (< index sample-size)
+                      (setf (svref sample index)
+                            property)))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+(defun sample-tree (sample-to a-tree
+                    &optional (random-state *random-state*))
+  "Return vector of random nodes in `a-tree'."
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type list a-tree))
+  (flet ((this-sampler (sample-size sample)
+           (let ((counter 0))
+             (flet ((node-test (node other)
+                      (declare (ignore other))
+                      (if (< counter sample-size)
+                          (setf (svref sample counter)
+                                node)
+                          (let ((index (random counter random-state)))
+                            (when (< index sample-size)
+                              (setf (svref sample index)
+                                    node))))
+                      (incf counter 1)
+                      t))
+               (tree-equal a-tree a-tree
+                           :test #'node-test)
+               sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+
+;;; Random list items
+
+(defun random-list-item (a-list
+                         &optional (random-state *random-state*))
+  "Return random item in `a-list'."
+  (let ((sample (sample-list 1 a-list random-state)))
+    (svref sample 0)))
+
+(defun random-alist-assoc (an-alist
+                           &optional (random-state *random-state*))
+  "Return random association (key-datum pair) in `a-alist'."
+  (let ((sample (sample-list 1 an-alist random-state)))
+    (svref sample 0)))
+
+(defun random-plist-property (a-plist
+                              &optional (random-state *random-state*))
+  "Return random property (indicator-value pair) in `a-plist'."
+  (let ((sample (sample-plist 1 a-plist random-state)))
+    (svref sample 0)))
+
+(defun random-tree-node (a-tree
+                         &optional (random-state *random-state*))
+  "Return random node in `a-tree'."
+  (let ((sample (sample-tree 1 a-tree random-state)))
+    (svref sample 0)))
+
+
+;;; Sample hash-table mappings
+
+(defun sample-hash-table (sample-to a-hash-table
+                          &optional (random-state *random-state*))
+  "Return vector of random mappings (key-value pairs) in `a-hash-table'."
+  (declare (type (or simple-vector (integer 1)) sample-to)
+           (type hash-table a-hash-table))
+  (flet ((this-sampler (sample-size sample)
+           (loop
+             for counter from 0
+             for a-key being each hash-key of a-hash-table
+               using (hash-value a-value)
+             if (< counter sample-size)
+               do (setf (svref sample counter)
+                        (list a-key a-value))
+             else
+               do (let ((index (random counter random-state)))
+                    (when (< index sample-size)
+                      (setf (svref sample index)
+                            (list a-key a-value))))
+             finally (return sample))))
+    (etypecase sample-to
+      ((integer 1) (this-sampler sample-to
+                                 (make-array sample-to)))
+      (simple-vector (this-sampler (length sample-to)
+                                   sample-to)))))
+
+
+;;; Random hash-table mapping
+
+(defun random-hash-table-mapping (a-hash-table
+                                  &optional (random-state *random-state*))
+  "Return random mapping (key-value pair) in `a-hash-table'."
+  (let ((sample (sample-hash-table 1 a-hash-table random-state)))
+    (svref sample 0)))
+
+
 ;;; Shuffle sequences
 
 (defun nshuffle (a-sequence &optional (random-state *random-state*))
   "Destructively Knuth shuffle `a-sequence'."
-  (let ((seqlen (length a-sequence)))
-    (dotimes (index seqlen a-sequence)
-      (rotatef (elt a-sequence index)
-               (elt a-sequence
-                    (+ index
-                       (random (- seqlen index)
-                               random-state)))))))
+  (etypecase a-sequence
+    (list (loop
+            for counter from 0
+            for index = (random (+ 2 counter) random-state)
+            for item in a-sequence
+            do (rotatef (elt a-sequence index)
+                        (elt a-sequence counter))
+            finally (return a-sequence)))
+    (sequence (loop
+                for counter from 0
+                for index = (random (+ 2 counter) random-state)
+                for element across a-sequence
+                do (rotatef (elt a-sequence index)
+                            (elt a-sequence counter))
+                finally (return a-sequence)))))
 
 (defun shuffle (a-sequence &optional (random-state *random-state*))
   "Return a Knuth shuffled copy of `a-sequence'."
