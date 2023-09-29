51da6a638d44701a5a46953ba5c71c13f1a428ca
William Clifford <wobh@yahoo.com>
2023-09-28 21:02:25 -0400

    reset tests
    - delete tests (to be replaces with better tests)
    - fix system test definition

--- chance-test.lisp
+++ chance-test.lisp
@@ -2,9 +2,9 @@
 
 (defpackage #:org.wobh.common-lisp.tools.chance-test
   (:nicknames #:chance-test)
-  (:use #:common-lisp)
-  ;; (:use #:chance) ;; maybe?
   (:local-nicknames (#:chance #:org.wobh.common-lisp.tools.chance))
+  (:use #:common-lisp)
+  (:export #:test-all)
   (:documentation "ORG.WOBH.COMMON-LISP.TOOLS.CHANCE-TEST
 
 Basic tests for chance. If this file loads without errors, the
@@ -12,70 +12,9 @@ tests passed."))
 
 (in-package #:org.wobh.common-lisp.tools.chance-test)
 
-(let* ((rstate (make-random-state t))
-       (*random-state* (make-random-state rstate))
-       (expect (random 6)))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (subject (chance:random-whole 6)))
-    (assert (= expect
-               (1- subject))))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (subject (chance:random-range 3 8)))
-    (assert (= expect
-               (+ -3 subject))))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (indyvar 6)
-         (subject (chance:random-digit indyvar)))
-    (assert (= expect (digit-char-p subject indyvar)))))
-
-(let* ((rstate (make-random-state t))
-       (*random-state* (make-random-state rstate))
-       (expect (random 26)))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (subject (chance:random-alpha)))
-    (assert (= expect
-               (+ -10 (digit-char-p subject 36))))))
-
-(let* ((rstate (make-random-state t))
-       (*random-state* (make-random-state rstate))
-       (expect (random 4)))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (indyvar '(:foo :bar :baz :qux))
-         (subject (chance:random-nth indyvar)))
-    (assert (= expect
-               (position subject indyvar))))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (indyvar "abcd")
-         (subject (chance:random-char indyvar)))
-    (assert (= expect
-               (position subject indyvar))))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (indyvar #(:foo :bar :bax :qux))
-         (subject (chance:random-svref indyvar)))
-    (assert (= expect
-               (position subject indyvar))))
-
-  (let* ((*random-state* (make-random-state rstate))
-         (indyvar #(:foo :bar :bax :qux))
-         (subject (chance:random-position indyvar)))
-    (assert (= expect
-               subject))))
-
-;; FIXME: figure out how to test `nshuffle'.
-;; (let* ((rstate (make-random-state t))
-;;        (*random-state* (make-random-state rstate))
-;;        (indyvar #(:foo :bar :baz :qux))
-;;        (expect (loop
-;;                   for i from 0 below (length indyvar)
-;;                   collect (+ i (random i)))))
-
-;;   (let* ((*random-state* (make-random-state rstate))
-;;          (subject (nshuffle indyvar)))
-;;     (assert ())))
+(defun test-all ()
+  (loop
+    with tests = '()
+    for test in tests
+    do (funcall test)
+    finally (return t)))
--- org.wobh.common-lisp.tools.chance.asd
+++ org.wobh.common-lisp.tools.chance.asd
@@ -5,14 +5,14 @@
   :version "0.0.2"
   :license "Copyright © 2016-2023 William Clifford All rights reserved."
   :author "William Clifford <will@wobh.org>"
+  :in-order-to ((test-op (test-op "org.wobh.common-lisp.tools.chance/test")))
   :components ((:file "chance")
-               (:file "chance-user"))
-  :in-order-to ((test-op (test-op "org.wobh.common-lisp.tools.chance/test"))))
+               (:file "chance-user"
+                      :depends-on ("chance"))))
 
 (defsystem "org.wobh.common-lisp.tools.chance/test"
   :description "Tests for the chance system"
   :depends-on ("org.wobh.common-lisp.tools.chance")
-  :components ((:file "chance-test"))
-  :perform (test-op (o c) (symbol-call :common-lisp
-                                       'load
-                                       "chance-test")))
+  :perform (test-op (o c) (symbol-call 'chance-test
+                                       'test-all))
+  :components ((:file "chance-test")))
